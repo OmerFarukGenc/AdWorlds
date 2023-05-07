5f1883e36769e483a00184527b10d07bd72040f3
TunaB <59950729+TunaB@users.noreply.github.com>
2023-05-07 12:56:19 +0300

    Update Class1.cs

--- UnityPlugin/AdWorldsPlugin/Class1.cs
+++ UnityPlugin/AdWorldsPlugin/Class1.cs
@@ -17,7 +17,7 @@ namespace AdWorldsPlugin
     {
         private static readonly HttpClient client = new HttpClient();
         public int c;
-        //string url = "https://oaks-advantages-coordinated-voters.trycloudflare.com/api/getRandomAddvert";
+        string url = "https://oaks-advantages-coordinated-voters.trycloudflare.com";
         private Dictionary<string, string> AdIDs = new Dictionary<string, string>();
         public async Task<String> exampleReqToReceiveAd() {
             var response = await client.GetAsync("http://localhost:3000/api/custAddvert/2");
@@ -93,12 +93,12 @@ namespace AdWorldsPlugin
         
         public IEnumerator runtimeGet(Transform parentTransform, System.Action<UnityEngine.Object,String> callback)
         {
-            string path = "Assets/Resources/backendurl.txt";
+            /*string path = "Assets/Resources/backendurl.txt";
 
             StreamReader reader = new StreamReader(path);
             String url=reader.ReadToEnd();
             MonoBehaviour.print(url);
-            reader.Close();
+            reader.Close();*/
             string randomUrl=url;
             string adID = "";
             using (WWW web = new WWW(url + "/api/getRandomAdId"))
