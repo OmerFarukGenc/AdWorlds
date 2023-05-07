63f95ec8f223b7da9374031db04cfba57deac1b1
TunaB <59950729+TunaB@users.noreply.github.com>
2023-05-07 12:57:50 +0300

    Add files via upload

--- UnityExample/Scripts/test.cs
+++ UnityExample/Scripts/test.cs
@@ -2,14 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using AdWorldsPlugin;
-
 public class test : MonoBehaviour
 {
+    AdWorlds plugin;
     // Start is called before the first frame update
     void Start()
     {
-        AdWorlds utils = new AdWorlds();
-        StartCoroutine(utils.runtimeGet(transform));
+        plugin = new AdWorlds();
+        plugin.generateObj(this);
     }
 
     // Update is called once per frame
